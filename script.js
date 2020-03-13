408e8bc8ac2b0e460c8d32d717444a99a45e0d39
Irina Belousova <irinabelousova11@gmail.com>
2020-03-13 23:00:00 +0400

    CSS tyle was finished, Some JS was added

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let musicContainer = document.getElementById("musicContainer");
 
+let playBtn = document.getElementById("play");
+let prevBtn = document.getElementById("prev");
+let nextBtn = document.getElementById("next");
+
+let progress = document.getElementById("progress");
+let audio = document.getElementById("audio");
+let title = document.getElementById("title");
+
+let songs = [
+  "Bratia Stereo - Ayayay (ft. Tony Tonite)",
+  "Isaac Chambers feat. Bluey Moon-Moonlight"
+];
+
+let songIndex = 1;
+
+loadSong(songs[songIndex]);
+
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+}
+
+playBtn.addEventListener("click", () => {
+    if
+});
