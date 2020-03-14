f3f04b027e939b103a095c8fa00dcaf1ce2fe15e
Irina Belousova <irinabelousova11@gmail.com>
2020-03-14 23:00:00 +0400

    Add js logic for prev,next, progress bar, timer

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ let playBtn = document.getElementById("play");
 let prevBtn = document.getElementById("prev");
 let nextBtn = document.getElementById("next");
 
-let progress = document.getElementById("progress");
+let progress = document.getElementById("runner");
 let audio = document.getElementById("audio");
 let title = document.getElementById("title");
+let time = document.querySelector(".time");
 
 let songs = [
   "Bratia Stereo - Ayayay (ft. Tony Tonite)",
@@ -22,6 +23,80 @@ function loadSong(song) {
   audio.src = `music/${song}.mp3`;
 }
 
+function playSong() {
+  playBtn.classList.remove("fa-play");
+  playBtn.classList.add("fa-pause");
+  playBtn.classList.add("active");
+  audio.play();
+}
+
+function pauseSong() {
+  playBtn.classList.remove("fa-pause");
+  playBtn.classList.remove("active");
+  playBtn.classList.add("fa-play");
+  audio.pause();
+}
+
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function setTimer() {
+  let seconds = Math.floor(audio.currentTime % 60);
+  let minutes = Math.floor(audio.currentTime / 60);
+
+  time.innerText =
+    ("0" + minutes).substr(-2) + ":" + ("0" + seconds).substr(-2);
+}
+
+function updateProgress(event) {
+  let { duration, currentTime } = event.srcElement;
+  let progressPercent = (currentTime / duration) * 100;
+
+  progress.style.width = `${progressPercent}%`;
+  setTimer();
+}
+
+function setProgress(element) {
+  let width = this.clientWidth;
+  let clickX = element.offsetX;
+  let duration = audio.duration;
+  // console.log(clickX);
+  audio.currentTime = (clickX / width) * duration;
+}
+
 playBtn.addEventListener("click", () => {
-    if
+  let isPlaying = playBtn.classList.contains("active");
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
 });
+
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+
+audio.addEventListener("timeupdate", updateProgress);
+audio.addEventListener("ended", nextSong);
+
+progress.parentElement.addEventListener("click", setProgress);
